6463b1d27dd2e94b5447de337d7644df95660d0f
TuxMan531 <jaxf2005@gmail.com>
2025-07-09 15:15:01 -0500

    input checking

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 function runCalc() {
-    const amount = document.getElementById("Amount").value;
-    const year = document.getElementById("Year").value;
+    const amount = document.getElementById("Amount");
+    const year = document.getElementById("Year");
 
-    alert(amount);
-    alert(year);
+    let numAmount;
+    let beReady = 0;
+
+    console.log(amount.value);
+    //console.log(year);
+
+ // valid input check
+    numAmount = parseInt(amount.value)
+    yearAmount = parseInt(year.value)
+
+    if (Number.isInteger(numAmount)) {
+        console.log(numAmount + " passses number check");
+        if (numAmount > 0) {
+            console.log(numAmount + " passses positive check");
+            beReady += 1;
+            console.log("added one, for amount")
+        } else {
+            amount.value = "Enter a positive number";
+        }
+    } else {
+        console.log(numAmount);
+        console.log("Not a number")
+        amount.value = "No, enter a number";
+    }
+
+
+    if (Number.isInteger(yearAmount)) {
+        console.log(yearAmount + " passses number check");
+        if (yearAmount > 1799) {
+            console.log(yearAmount + " valid year check");
+            beReady += 1;
+            console.log("added one, for year")
+        } else {
+            year.value = "Enter a year 2025 - 1800";
+        }
+    } else {
+        console.log(yearAmount);
+        console.log("Not a number")
+        year.value = "No, enter a number";
+    }
+
+    ////////
+
+    if (beReady == 2) {
+
+
+
+        
+    }
+    
 }
--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    resizable: false
   })
   win.loadFile('index.html')
   win.setMenu(null)
+  win.webContents.openDevTools({ mode: 'detach' });
 }
 
 app.whenReady().then(() => {
